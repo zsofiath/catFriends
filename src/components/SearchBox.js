d6a0aaf5e9abe397436eabd60d48c882c5058919
zsofi <zsofiath@gmail.com>
2021-12-21 18:49:52 +0100

    change input color

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ export default function SearchBox({searchField, searchChange}) {
     return (
         <div>
             <input
-            className="b--light-blue bg-dark-blue blue"
+            className="b--light-blue bg-light-blue black"
             type="search"
             placeholder="search"
             onChange={searchChange}
